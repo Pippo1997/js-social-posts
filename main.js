43e21304d837f735f3bbc4d08e177200b5428e5b
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-12-15 15:51:00 +0100

    milestone 2

--- main.js
+++ main.js
@@ -56,4 +56,42 @@ const posts = [
     }
 ];
 
-console.log(posts)
+let itemContent = ``;
+
+posts.forEach((element) => {
+
+    itemContent += `<div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`
+    
+});
+
+const cardPreview = document.getElementById(`container`)
+cardPreview.innerHTML += itemContent;
